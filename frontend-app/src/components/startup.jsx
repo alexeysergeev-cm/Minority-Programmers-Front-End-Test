8117b0d9ab32c1aa1e4264f62ca51852b765c4f5
AlexeyS <xengawe@gmail.com>
2021-05-18 21:41:10 -0700

    do incubator index item

--- frontend-app/src/components/startup.jsx
+++ frontend-app/src/components/startup.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styling/startup.css'
+import { Link } from 'react-router-dom';
 const incData = require('../data/incubator.json');
 
 class StartUp extends React.Component{
@@ -47,40 +48,63 @@ class StartUp extends React.Component{
               <button>Fund Startup</button>
             </div>
           </div>
-          <div className='s-sidebar'>
-            <div>{founded}</div>
-            <hr className="Solid" />
-            <div>
-              <h1>Website</h1>
-              {website}
+          <div className='start-main'>
+            <div className='s-sidebar'>
+              <div>{founded}</div>
+              <hr className="Solid" />
+              <div>
+                <h1>Website</h1>
+                {website}
+              </div>
+              <div>
+                <h1>Location</h1>
+                {location}
+              </div>
+              <div>
+                <h1>Team Size</h1>
+                {teamsize}
+              </div>
+              <hr className="Solid" />
+              <div>
+                <h1>Meet the team</h1>
+                {/* {team.map(member => (
+                  <h1>{member}</h1>
+                ))} */}
+                {teamSection}
+              </div>
+              <hr className="Solid" />
+              <div>
+                <h1>Tags</h1>
+                {tagSection}
+              </div>
             </div>
-            <div>
-              <h1>Location</h1>
-              {location}
+            <div style={{'overflow': 'auto'}}>
+              <div className='s-about'>
+                <div>
+                  <h1>About startup</h1>
+                  <div>{about}</div>
+                </div>
+                <hr className="Solid" />
+                <div>
+                  <h1>Vision</h1>
+                  <div>{vision}</div>
+                </div>
+              </div>
+              <div className='s-footer'>
+                <h1>Related startups</h1>
+                <div className='related-cont'>
+                  {incData.map(startUp => {
+                    return (
+                      <Link key={startUp.id} to={`/incubator/${startUp.id}`}>
+                        <div className="related-item">
+                          <h1>{startUp.name}</h1>
+                        </div>
+                      </Link>
+                    )
+                  })}
+                </div>
+              </div>
             </div>
-            <div>
-              <h1>Team Size</h1>
-              {teamsize}
-            </div>
-            <hr className="Solid" />
-            <div>
-              <h1>Meet the team</h1>
-              {/* {team.map(member => (
-                <h1>{member}</h1>
-              ))} */}
-              {teamSection}
-            </div>
-            <hr className="Solid" />
-            <div>
-              <h1>Tags</h1>
-              {tagSection}
-            </div>
-          </div>
-          <div className='s-main'>
-
-          </div>
-          <div className='s-footer'>
-
           </div>
         </div>
       </div>
