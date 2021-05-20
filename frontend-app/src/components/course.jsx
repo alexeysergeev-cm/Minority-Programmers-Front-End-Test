d5769076b49c0cfb50486b6bd21ec287ad824851
AlexeyS <xengawe@gmail.com>
2021-05-20 16:38:17 -0700

    add id to courses

--- frontend-app/src/components/course.jsx
+++ frontend-app/src/components/course.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import '../styling/course.css';
+const courses = require('../data/courses.json');
 
 class Course extends React.Component{
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  componentDidMount(){
+    let course;
+
+    for(let i = 0; i < courses.length; i++){
+      if (courses[i]){
+        if(courses[i].id === this.props.match.params.courseId) {
+          course = courses[i];
+          break;
+        }
+      }
+    }
+
+    this.setState(course)
+  }
 
   render(){
     return(
-      <div>COURSE</div>
+      <div className='course-page'>
+        <div className='course-side'>
+
+        </div>
+        <div className='course-main'>
+          <div className='course-head'>
+
+          </div>
+          <div className='course-info'>
+
+          </div>
+        </div>
+      </div>
     )
   }
 }
