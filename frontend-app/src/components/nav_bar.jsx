19fedce78eb33229edc0a5c0ec6c67ab612d415f
AlexeyS <xengawe@gmail.com>
2021-05-20 15:32:58 -0700

    change portfolio and linkedin links and set target to _blank

--- frontend-app/src/components/nav_bar.jsx
+++ frontend-app/src/components/nav_bar.jsx
@@ -5,18 +5,18 @@ const NavBar = () => (
   <div className='nav-bar'>
     <div className='left-nav'>
       <div className='logo'>
-        <a href="https://minorityprogrammers.com/">
+        <a href="https://minorityprogrammers.com/" target="_blank">
           <img style={{'width': '128px'}} src="https://minorityprogrammers.com/assets/images/mp_gradient_rock.svg" />
         </a>
       </div>
       <div className='social'>
-        <a href="https://alexeysergeev-cm.github.io/">
-          <img style={{'height': '35px'}} src="https://img.icons8.com/fluent-systems-filled/48/000000/portfolio.png"/>
+        <a href="https://alexeysergeev-cm.github.io/" target="_blank">
+          <img src="https://img.icons8.com/ios/28/000000/resume-website.png"/>
         </a>
-        <a href="https://github.com/alexeysergeev-cm">
-          <img style={{'height': '40px'}} src="https://img.icons8.com/windows/32/000000/github-squared.png"/>
+        <a href="https://github.com/alexeysergeev-cm" target="_blank">
+          <img src="https://img.icons8.com/material-sharp/28/000000/github.png"/>
         </a>
-        <a href="https://www.linkedin.com/in/alexey-sergeev-cm/">
+        <a href="https://www.linkedin.com/in/alexey-sergeev-cm/" target="_blank">
           <img src="https://img.icons8.com/metro/26/000000/linkedin.png"/>
         </a>
       </div>
