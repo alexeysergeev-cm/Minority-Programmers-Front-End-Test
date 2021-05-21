7e88b47839c8c5972ea8b2ad8e83a5fea910655f
AlexeyS <xengawe@gmail.com>
2021-05-20 20:20:04 -0700

    complete sidebar for courses

--- frontend-app/src/components/course.jsx
+++ frontend-app/src/components/course.jsx
@@ -19,15 +19,40 @@ class Course extends React.Component{
         }
       }
     }
-
+    // debugger
     this.setState(course)
   }
 
   render(){
+    const { name, weeks } = this.state;
+    // debugger
+
+    let w;
+    if(weeks){
+      w = weeks.map((week, i) => {
+        return( 
+          <div>
+            <div>Week {i+1}</div>
+            {week.map((wModule,i) => {
+              if(i === week.length - 1){
+                return (<p><img src="https://img.icons8.com/fluent/20/000000/checked-radio-button.png"/>{wModule}</p>)
+
+              }
+              return (<p><img src="https://img.icons8.com/flat-round/18/000000/checkmark.png"/>{wModule}</p>)
+            }
+            
+            )}
+          </div>
+        )
+      })
+    }
+
     return(
       <div className='course-page'>
         <div className='course-side'>
-
+          <img src="https://img.icons8.com/carbon-copy/48/ffffff/left.png"/>
+          <h2>{name}</h2>
+          {w}
         </div>
         <div className='course-main'>
           <div className='course-head'>
